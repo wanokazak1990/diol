2547a389b4f86356b8bdd096605ae620c61dd5eb
Ivan <wanokazak1990@yandex.ru>
2020-06-17 17:54:09 +0300

    Сортировка сначало имещиеся, отсутствующие в конце

--- app/Services/ProductSort.php
+++ app/Services/ProductSort.php
@@ -2,12 +2,13 @@
 namespace App\Services;
 use App\Models\Product;
 use Illuminate\Http\Request;
-
+use DB;
 Class ProductSort 
 {
 	public function getSorted($array = [])
 	{		
-		$query = Product::select('*');
+		$query = Product::select('id','name','description','picture','price','count',DB::raw('if(count>0,1,0) as available'))
+			->orderBy('available','desc');
 		if(isset($array['name']) && isset($array['value'])) :
 			$query->orderBy($array['name'],$array['value']);			
 		endif;
